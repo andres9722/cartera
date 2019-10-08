4a47dc44951f96094cf00a683b27b8200d1ed216
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2019-10-08 09:20:34 -0500

    Updates

---
+++ precache-manifest.ea6d5c278297bd20b85ac94bfb01918e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86636209bb23d9c32870e0c4c4d5d9ba",
+    "url": "/cartera/static/media/loginbg.86636209.jpg"
+  },
+  {
+    "revision": "63c2062c15265e4661ff",
+    "url": "/cartera/static/js/runtime~main.63c2062c.js"
+  },
+  {
+    "revision": "80022de8e95e0dc352e1",
+    "url": "/cartera/static/js/main.80022de8.chunk.js"
+  },
+  {
+    "revision": "09ac022dff1ed90bde78",
+    "url": "/cartera/static/js/1.09ac022d.chunk.js"
+  },
+  {
+    "revision": "80022de8e95e0dc352e1",
+    "url": "/cartera/static/css/main.c263e512.chunk.css"
+  },
+  {
+    "revision": "09ac022dff1ed90bde78",
+    "url": "/cartera/static/css/1.5417b344.chunk.css"
+  },
+  {
+    "revision": "817c21c3371af39f499d870327d14073",
+    "url": "/cartera/index.html"
+  }
+];
